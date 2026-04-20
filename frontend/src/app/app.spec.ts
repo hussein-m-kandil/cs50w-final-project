21facbf39517405b10c5b942afbd9e63475c6389
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-04-20 08:28:31 +0200

    Configure frontend to use tailwindcss

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,23 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { render, RenderComponentOptions, screen } from '@testing-library/angular';
+import { environment } from '../environments';
 import { App } from './app';
 
+const renderComponent = (options: RenderComponentOptions<App> = {}) => {
+  return render(App, options);
+};
+
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, provetrina');
+  it('should display the app title', async () => {
+    await renderComponent();
+    const name = environment.title;
+    expect(screen.getByRole('heading', { name })).toBeVisible();
   });
 });
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('provetrina');
+  protected readonly title = environment.title;
 }
