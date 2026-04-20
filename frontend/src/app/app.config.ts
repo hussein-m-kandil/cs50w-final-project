3306ec115d0b79ba52b914a25701d72e7876d4d1
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-04-20 08:57:08 +0200

    Configure frontend to use PrimeNG

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
+import Material from '@primeuix/themes/material';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      inputVariant: 'filled',
+      ripple: true,
+      theme: {
+        preset: Material,
+        options: {
+          darkModeSelector: '.app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+        },
+      },
+    }),
+  ],
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments';
+import { MessageService } from 'primeng/api';
+import { Button } from 'primeng/button';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toast, Button],
   templateUrl: './app.html',
+  providers: [MessageService],
 })
 export class App {
   protected readonly title = environment.title;
+
+  protected toggleDarkMode() {
+    document.documentElement.classList.toggle('app-dark');
+  }
 }
