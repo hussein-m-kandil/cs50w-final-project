3636931fbeac2878ea5fb8f324009dcb41c4a466
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-04-20 09:30:05 +0200

    Add frontend interceptor to retry failed requests

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,27 +1,36 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { providePrimeNG } from 'primeng/config';
+
 import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { retryingInterceptor } from './retrying-interceptor';
+
+import { PrimeNGConfigType, providePrimeNG } from 'primeng/config';
 import Material from '@primeuix/themes/material';
 
-import { routes } from './app.routes';
+export const interceptors = [retryingInterceptor];
+
+export const primengConfig: PrimeNGConfigType = {
+  inputVariant: 'filled',
+  ripple: true,
+  theme: {
+    preset: Material,
+    options: {
+      darkModeSelector: '.app-dark',
+      cssLayer: {
+        name: 'primeng',
+        order: 'theme, base, primeng',
+      },
+    },
+  },
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors(interceptors)),
     provideBrowserGlobalErrorListeners(),
+    providePrimeNG(primengConfig),
     provideRouter(routes),
-    providePrimeNG({
-      inputVariant: 'filled',
-      ripple: true,
-      theme: {
-        preset: Material,
-        options: {
-          darkModeSelector: '.app-dark',
-          cssLayer: {
-            name: 'primeng',
-            order: 'theme, base, primeng',
-          },
-        },
-      },
-    }),
   ],
 };
