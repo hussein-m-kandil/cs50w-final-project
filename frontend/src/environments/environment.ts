269b2c95cace01dbb79f6724439b1f16b1b66144
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-04-20 07:50:55 +0200

    Customize Angular testing and environments config

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,3 @@
+import { environment as devEnv } from './environment.development';
+
+export const environment = { ...devEnv };
---
+++ frontend/src/test/setup.ts
@@ -0,0 +1,30 @@
+import { afterAll, beforeEach, vi } from 'vitest';
+
+beforeEach(() => {
+  vi.unstubAllGlobals();
+  vi.stubGlobal(
+    'ResizeObserver',
+    vi.fn(
+      class ResizeObserverMock {
+        disconnect = vi.fn();
+        unobserve = vi.fn();
+        observe = vi.fn();
+      },
+    ),
+  );
+  vi.stubGlobal(
+    'IntersectionObserver',
+    vi.fn(
+      class IntersectionObserverMock {
+        takeRecords = vi.fn();
+        disconnect = vi.fn();
+        unobserve = vi.fn();
+        observe = vi.fn();
+      },
+    ),
+  );
+});
+
+afterAll(() => {
+  vi.unstubAllGlobals();
+});
