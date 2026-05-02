06a1022b68046386382287b9ed3aef45b568625b
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-05-02 22:21:54 +0300

    Edit frontend types

--- frontend/src/app/app.types.ts
+++ frontend/src/app/app.types.ts
@@ -1,8 +1,8 @@
 export type ValueOf<T extends object> = T[keyof T];
 
 export interface ListResponse<T> {
-  previous: number | null;
-  next: number | null;
+  previous: string | null;
+  next: string | null;
   count: number;
   results: T[];
 }
